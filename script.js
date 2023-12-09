c7b2078ab8ab90d0323e53eba192f14cd5b3e610
kkunamu <143686607+kkunamu@users.noreply.github.com>
2023-12-09 20:12:30 +0000

    okay

--- script.js
+++ script.js
@@ -1,92 +1,69 @@
-    function showAssignments() {
-        var userNameInput = document.getElementById("userNameInput");
-        var userName = userNameInput.value.trim().toLowerCase();
+function processName() {
+  const userName = document.getElementById('userNameInput').value.trim();
 
-        if (userName !== "") {
-            var fontSize = Math.min(36, 120 / Math.max(1, userName.length));
+  if (userName !== '') {
+    localStorage.setItem('userName', userName);
 
-            var welcomeMessage = document.getElementById("welcomeMessage");
-            welcomeMessage.innerHTML = "Welcome " + userName;
-            welcomeMessage.style.fontSize = fontSize + "px";
+    document.getElementById('overlay').style.display = 'none';
+    document.querySelector('.content-container').style.display = 'flex';
+    document.querySelector('.welcome-container').style.display = 'flex';
+    document.querySelector('.menu-container').style.display = 'flex';
+    const welcomeMessage = document.getElementById('welcomeMessage');
+    welcomeMessage.innerText = `˚ʚWelcome, ${userName}ɞ˚`;
 
-            document.getElementById("overlay").style.display = "none";
-            document.getElementById("content").style.display = "block";
+    // Set font size based on the length of the username
+    welcomeMessage.style.fontSize = getFontSize(userName.length);
 
-            // Store the user's name in localStorage
-            localStorage.setItem("userName", userName);
+    console.log('Welcome', userName);
+  }
+}
 
-            document.getElementById("blurredBackground").style.display = "block";
-        }
+function getFontSize(length) {
+  if (length === 6) {
+    return '34px';
+  } else if (length === 5) {
+    return '36px';
+  } else if (length === 4) {
+    return '38px';
+  } else if (length === 3) {
+    return '40px';
+  } else if (length === 2) {
+    return '40px';
+  } else {
+    return '45px';
+  }
+}
+
+const storedUserName = localStorage.getItem('userName');
+if (storedUserName) {
+  document.getElementById('overlay').style.display = 'none';
+  document.querySelector('.content-container').style.display = 'flex';
+  document.querySelector('.welcome-container').style.display = 'flex';
+  document.querySelector('.menu-container').style.display = 'flex';
+  const welcomeMessage = document.getElementById('welcomeMessage');
+  welcomeMessage.innerText = `˚ʚWelcome back, ${storedUserName}ɞ˚`;
+  // Set font size based on the length of the username
+  welcomeMessage.style.fontSize = getFontSize(storedUserName.length);
+}
+
+document.getElementById('userNameInput').addEventListener('keyup', function (event) {
+    if (event.key === 'Enter') {
+        processName();
     }
+});
 
-    function toggleAssignments() {
-        var welcomeMessage = document.getElementById("welcomeMessage");
-        var arrow = document.getElementById("arrow");
-        var assignmentList = document.getElementById("assignmentList");
+function toggleTopNav() {
+    const topNavLeft = document.querySelector('.topnav-left');
+    const topNavRight = document.querySelector('.topnav-right');
 
-        welcomeMessage.style.display = "none";
-        arrow.style.display = "none"; // Hide the arrow during the assignment list
-        assignmentList.style.display = "block";
-        help.style.display = "block";
-        document.getElementById("blurredBackground").style.display = "block";
-    }
+    topNavLeft.classList.toggle('show');
+    topNavRight.classList.toggle('show');
+}
 
-    function resetState() {
-        var overlay = document.getElementById("overlay");
-        var welcomeMessage = document.getElementById("welcomeMessage");
-        var arrow = document.getElementById("arrow");
-        var assignmentList = document.getElementById("assignmentList");
-        var userNameInput = document.getElementById("userNameInput");
+document.getElementById('homepaw').addEventListener('click', function() {
+    toggleTopNav();
+});
 
-        overlay.style.display = "flex";
-        welcomeMessage.style.display = "block";
-        arrow.style.display = "block"; // Show the arrow during the welcome message
-        assignmentList.style.display = "none";
-        userNameInput.value = "";
-
-        // Check if the user's name is stored in localStorage
-        var storedUserName = localStorage.getItem("userName");
-        if (storedUserName) {
-            // Skip the entire overlay, welcome message, and show the assignment list
-            overlay.style.display = "none";
-            document.getElementById("content").style.display = "block";
-            welcomeMessage.style.display = "none";
-            arrow.style.display = "none"; // Hide the arrow when the assignment list is displayed
-            assignmentList.style.display = "block";
-            help.style.display = "block";
-            document.getElementById("blurredBackground").style.display = "block";
-        } else {
-            // Delayed prompt messages
-            setTimeout(function () {
-                document.getElementById("promptMessage").innerHTML = "What's your name?";
-                userNameInput.style.display = "block";
-                document.getElementById("pressEnter").style.display = "block";
-                arrow.style.display = "block"; // Show the arrow after the welcome message
-                help.style.display = "none";
-                document.getElementById("blurredBackground").style.display = "none";
-            }, 3000);
-        }
-    }
-    document.addEventListener('DOMContentLoaded', function () {
-        var links = document.querySelectorAll('a'); // Adjust the selector based on your links
-        var loadingOverlay = document.getElementById('loadingOverlay');
-
-        links.forEach(function (link) {
-            link.addEventListener('click', function (event) {
-                event.preventDefault();
-
-        // Show loading overlay
-        loadingOverlay.style.display = 'flex';
-
-            var href = this.getAttribute('href');
-
-    // Redirect to the new webpage after a delay (adjust the delay as needed)
-    setTimeout(function () {
-        window.location.href = href;
-                }, 1000); // Adjust the delay as needed
-            });
-        });
-    });
-    // Call the resetState function on page load
-    resetState();
-
+function navigateTo(url) {
+  window.location.href = url;
+}
